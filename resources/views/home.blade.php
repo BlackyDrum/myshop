5b8bdc2bcbca54e3a42805631b55f03700ce1123
BlackyDrum <ganiaytan1010@web.de>
2023-01-04 23:10:21 +0100

    changed styling and design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,12 +13,12 @@
     <script src="https://cdn.tailwindcss.com"></script>
     @vite('resources/css/app.css')
 </head>
-<body onload="init()" class="bg-gray-200">
+<body onload="init()" class="bg-gray-200 font-mono">
 @include('cookie-consent::index')
 <header class="bg-gray-300">
     <div class="h-20  flex flex-nowrap">
         <div class="m-auto max-lg:ml-10">
-            <a href="/"><img src="img/zalin-2.png" alt="Logo" class="w-16"></a>
+            <a href="/"><img src="img/logo-home.png" alt="Logo" class="w-16"></a>
         </div>
         <nav class="mr-80 mt-auto mb-auto flex gap-6 text-gray-800 max-lg:hidden">
             <div class="underline decoration-2 decoration-green-500 underline-offset-8 font-bold">
@@ -27,9 +27,7 @@
             <div class="hover:underline decoration-2 decoration-green-500 underline-offset-8 ">
                 <a href="/shop">Shop</a>
             </div>
-            <div class="hover:underline decoration-2 decoration-green-500 underline-offset-8 ">
-                <a href="mission">Mission</a>
-            </div>
+
             <div class="hover:underline decoration-2 decoration-green-500 underline-offset-8 ">
                 <a href="contact">Kontakt</a>
             </div>
@@ -38,6 +36,9 @@
             </div>
             <div class="hover:text-green-500">
                 <i class="fa-solid fa-cart-shopping"></i>
+            </div>
+            <div class="hover:text-green-500">
+                <i class="fa-solid fa-user"></i>
             </div>
         </nav>
         <nav class="mr-40 max-sm:mr-10 mt-auto mb-auto flex gap-6 text-gray-800 lg:hidden grid grid-cols-3">
@@ -51,7 +52,7 @@
                 <button id="nav" onclick="navbar()"><i class="fa-solid fa-bars scale-125"></i></button>
             </div>
         </nav>
-        <div id="navbar" class="bg-gray-400 fixed top-20 w-full transition-all lg:hidden">
+        <div id="navbar" class="bg-gray-400 fixed top-20 w-full transition-all lg:hidden z-20">
             <div id="navitems" class="h-full text-gray-800 text-center grid grid-cols-1 grid-rows-4  place-items-center">
                 <div>
                     <a href="/" class="font-bold">Willkommen</a>
@@ -60,7 +61,7 @@
                     <a href="/shop" class="hover:underline decoration-2 hover:font-bold underline-offset-8">Shop</a>
                 </div>
                 <div>
-                    <a href="/mission" class="hover:underline decoration-2 hover:font-bold underline-offset-8">Mission</a>
+                    <a href="/mission" class="hover:underline decoration-2 hover:font-bold underline-offset-8">Anmelden</a>
                 </div>
                 <div>
                     <a href="/contact" class="hover:underline decoration-2 hover:font-bold underline-offset-8">Kontakt</a>
@@ -69,48 +70,80 @@
         </div>
     </div>
 </header>
-<main class="mt-5">
-    <div>
-        <img src="img/header-1.jpg" alt="Header" class="w-full">
+<main class="mt-3">
+    <!-- Slideshow container -->
+    <div class="slideshow-container">
+
+        <!-- Full-width images with number and caption text -->
+        <div class="mySlides fade relative">
+            <div class="numbertext">1 / 3</div>
+            <img src="img/banner-home-1.png" class="w-full h-full">
+
+        </div>
+
+        <div class="mySlides fade relative">
+            <div class="numbertext">2 / 3</div>
+            <img src="img/banner-home-1.png" class="w-full h-full">
+
+        </div>
+
+        <div class="mySlides fade relative">
+            <div class="numbertext">3 / 3</div>
+            <img src="img/banner-home-1.png" class="w-full">
+
+        </div>
+
+        <!-- Next and previous buttons -->
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
     </div>
+    <br>
+
+    <!-- The dots/circles -->
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+
     <div class="mt-10 flex flex-wrap flex-1 gap-10 justify-evenly">
         <a href="/products/gekochte-wassermelonenkerne">
         <div class="card w-96">
-            <img src="img/Gekochte-Wassermelonenkerne-0-300x300.png" class="w-full" alt="Gekochte Wassermelonenkerne">
+            <img src="img/denim-male-gefuetterte-stiefel-black.png" class="w-full" alt="denim-male-gefuetterte-stiefel-black">
             <div class="container">
-                <h4 class="text-lg mb-2"><b>Gekochte Wassermelonenkerne</b></h4>
-                <p class="mb-2 text-sm">5,98€ / kg</p>
-                <p class="text-green-700 text-lg font-bold">ab 2,99€</p>
+                <p class="mb-2 text-sm">Denim Male</p>
+                <h4 class="text-lg mb-2"><b>Gefütterte Stiefel</b></h4>
+                <p class="text-lg ">79.95€</p>
             </div>
         </div>
         </a>
         <a href="/products/geroestete-kuerbiskerne">
         <div class="card w-96">
-            <img src="img/KAVRULMUS-KABAK-CEKIRDEGI-300x300.png" class="w-full" alt="Geröstete Kürbiskerne">
+            <img src="img/denim-male-gefuetterte-stiefel-brown.png" class="w-full" alt="denim-male-gefuetterte-stiefel-brown">
             <div class="container">
-                <h4 class="text-lg mb-2"><b>Geröstete Kürbiskerne 200g</b></h4>
-                <p class="mb-2 text-sm">1.25€ / 100g</p>
-                <p class="text-green-700 text-lg font-bold">ab 2,49€</p>
+                <p class="mb-2 text-sm">Denim Male</p>
+                <h4 class="text-lg mb-2"><b>Gefütterte Stiefel</b></h4>
+                <p class="text-lg ">79.95€</p>
             </div>
         </div>
         </a>
         <a href="/products/geroestete-sonnenblumenkerne">
         <div class="card w-96">
-            <img src="img/Geröstet-und-gesalzen-Sonnenblumenkerne-0-300x300.png" class="w-full" alt="Geröstete Sonnenblumenkerne">
+            <img src="img/trekking-stiefel-green.png" class="w-full" alt="trekking-stiefel-green">
             <div class="container">
-                <h4 class="text-lg mb-2"><b>Geröstete Sonnenblumenkerne 200 g</b></h4>
-                <p class="mb-2 text-sm">1,04€ / 100g</p>
-                <p class="text-green-700 text-lg font-bold">ab 2,49€</p>
+                <p class="mb-2 text-sm">Denim Male</p>
+                <h4 class="text-lg mb-2"><b>Trekking Stiefel</b></h4>
+                <p class="text-lg "><span class="text-orange-700">62,99€</span> <span class="line-through">79,95€</span></p>
             </div>
         </div>
         </a>
         <a href="/products/geroestete-wassermelonenkerne">
         <div class="card w-96">
-            <img src="img/Geröstete-Wassermelonenkerne-300x300.png" class="w-full" alt="Geröstete Wassermelonenkerne">
+            <img src="img/sneaker-mit-applikationen-black.png" class="w-full" alt="sneaker-mit-applikationen-black">
             <div class="container ">
-                <h4 class="text-lg mb-2"><b>Geröstete Wassermelonenkerne 240 g</b></h4>
-                <p class="mb-2 text-sm">1,04€ / 100g</p>
-                <p class="text-green-700 text-lg font-bold">ab 2,49€</p>
+                <p class="mb-2 text-sm">Denim Male</p>
+                <h4 class="text-lg mb-2"><b>Sneaker mit Applikationen</b></h4>
+                <p class="text-lg ">69.95€</p>
             </div>
         </div>
         </a>
@@ -126,15 +159,15 @@
         <div class="">
             <i class="fa-solid fa-envelope scale-250 text-pink-600"></i>
         </div>
-        <div class="text-center mx-10">
-            Wir versprechen nur schonend behandelte Produkte: das originale Geschmackserlebnis, das die Menschen im Orient bereits seit Jahrtausenden lieben.
+        <div class="text-center mx-5">
+            Wir versprechen nur schonend behandelte Produkte: alle Produkte werden in der EU angefertig und verpackt.
         </div>
-        <div class="text-center mx-10">
+        <div class="text-center mx-5">
             Dass wir ohne Zwischenhändler arbeiten, führt noch zu einem weiteren Vorteil: Die Kosten, die wir hier einsparen, können wir als Preisvorteil direkt an dich und alle unsere Kunden weitergeben.
         </div>
-        <div class="text-center mx-10">
+        <div class="text-center mx-5">
 
-            Echte Freunde sagen einander auch, falls mal etwas nicht ganz rund läuft. Sollten Geschmack oder Frische dich also einmal nicht überzeugen, freuen wir uns über ehrliches Feedback.
+            Echte Freunde sagen einander auch, falls mal etwas nicht ganz rund läuft. Sollte Qualität dich also einmal nicht überzeugen, freuen wir uns über ehrliches Feedback.
         </div>
     </div>
 
@@ -155,7 +188,7 @@
             <div class="">
                 <i class="fa-solid fa-envelope scale-250 text-pink-600 my-5"></i>
             </div>
-            Echte Freunde sagen einander auch, falls mal etwas nicht ganz rund läuft. Sollten Geschmack oder Frische dich also einmal nicht überzeugen, freuen wir uns über ehrliches Feedback.
+            Echte Freunde sagen einander auch, falls mal etwas nicht ganz rund läuft. Sollte Qualität dich also einmal nicht überzeugen, freuen wir uns über ehrliches Feedback.
         </div>
     </div>
     <div class="w-full lg:h-40 h-72 text-center mt-10 grid grid-cols-1 grid-rows-4 gap">
