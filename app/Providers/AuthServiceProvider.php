9b8216d35066520bcf011f057f34c7be225a3abe
BlackyDrum <ganiaytan1010@web.de>
2023-01-12 23:38:01 +0100

    translated verification mail

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +26,21 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        $test = new MailMessage();
 
-        //
+
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->from('please-do-not-reply@my-shop.de','My-Shop')
+                ->greeting('Hallo!')
+                ->salutation(env('APP_NAME'))
+                ->subject('E-Mail-Adresse verifizieren')
+                ->line('Klicken Sie auf die Schaltfläche unten, um Ihre E-Mail-Adresse zu bestätigen.')
+                ->action('E-Mail-Adresse verifizieren', $url)
+                ->line("Wenn Sie kein Konto erstellt haben, sind keine weiteren Maßnahmen erforderlich.");
+
+
+        });
+
     }
 }
